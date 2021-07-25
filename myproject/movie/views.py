39733894ecb670dfeeb713414c6c3f97da3f3fd4
kingharong <91714@naver.com>
2021-07-25 11:33:21 +0900

    [ 김하연 ] 정렬 코드 추가

--- myproject/movie/views.py
+++ myproject/movie/views.py
@@ -15,7 +15,7 @@ def home(request):
     option = request.GET.get('option')
     if query:
       movies = Movie.objects.filter(title_kor__icontains=query)
-    if option:
+    if option and option in ['rating_aud','rating_cri','rating_net']:
       movies = Movie.objects.order_by('-'+option)
     paginator = Paginator(movies, 5) # blogs를 3개씩 쪼갠다
     page = request.GET.get('page') # 해당 정보가 오지 않아도 넘어간다
